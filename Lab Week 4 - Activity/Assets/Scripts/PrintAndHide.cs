5e652730d5a5d1def6a364318a977d3c5932f5f8
Ciaran Mulholland <ciaranpgm@hotmail.com>
2019-08-15 12:16:46 +1000

    100% completion of Lab 4

--- Lab Week 4 - Activity/Assets/Scripts/PrintAndHide.cs
+++ Lab Week 4 - Activity/Assets/Scripts/PrintAndHide.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     int i = 0;
+    int randomNum;
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        randomNum = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (tag == "Blue" && i == randomNum)
+        {
+            rend.enabled = !rend.enabled;
+            Debug.Log("Blue died at" + i);
+        }
+        else if (tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+            Debug.Log("Red died at" + i);
+        }
+
         Debug.Log(name + ": " + i);
         i++;
     }
