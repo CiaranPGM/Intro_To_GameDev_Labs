3f27f3d2c539e46caf1a19c8ee3e0f44525f2c83
Ciaran Mulholland <ciaranpgm@hotmail.com>
2019-08-31 15:37:38 +1000

    Add Lab 6, 50%

---
+++ Lab Week 6 - Activity/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    int lastTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0;
+            Debug.Log("SPACEBAR IS PRESSED");
+        }
+
+        if (Time.time > lastTime)
+            Debug.Log(lastTime++);
+    }
+}
