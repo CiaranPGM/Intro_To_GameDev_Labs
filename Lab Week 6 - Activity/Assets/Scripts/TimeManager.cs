e4ceebc2057fe487379328d41a5e7caeaa4c0fb4
Ciaran Mulholland <ciaranpgm@hotmail.com>
2019-09-01 02:12:29 -0400

    Did lab week 6, 80%

--- Lab Week 6 - Activity/Assets/Scripts/TimeManager.cs
+++ Lab Week 6 - Activity/Assets/Scripts/TimeManager.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    int lastTime;
+    private int lastTime;
+    private float timer;
+    private const float moveWait = 2.0f;
+
+    [SerializeField]
+    private Transform[] transArray;
     // Start is called before the first frame update
     void Start()
     {
-        lastTime = 0;
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 2.0f;
+        InvokeRepeating("MoveObjects", 2.0f, 2.0f);
+        ResetTime();
     }
 
     // Update is called once per frame
@@ -16,11 +25,55 @@ public class TimeManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Time.timeScale = 0;
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+            }
+            else
+            {
+                Time.timeScale = 0;
+            }
             Debug.Log("SPACEBAR IS PRESSED");
+        }else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ResetTime();
         }
 
-        if (Time.time > lastTime)
-            Debug.Log(lastTime++);
+        timer += Time.deltaTime;
+
+        if((int)timer >= lastTime)
+        {
+            Debug.Log((int)timer);
+            lastTime++;
+        }
+    }
+
+    private void ResetTime()
+    {
+        timer = 0;
+        lastTime = 0;
+    }
+
+    private void MoveObjects()
+    { 
+        if (transArray[0].position.x == 2 && transArray[0].position.y == 1)
+        {
+            transArray[0].SetPositionAndRotation(new Vector3(2, -1, 0), new Quaternion());
+            transArray[1].SetPositionAndRotation(new Vector3(-2, 1, 0), new Quaternion());
+
+        }else if(transArray[0].position.x == 2 && transArray[0].position.y == -1)
+        {
+            transArray[0].SetPositionAndRotation(new Vector3(-2, -1, 0), new Quaternion());
+            transArray[1].SetPositionAndRotation(new Vector3(2, 1, 0), new Quaternion());
+
+        }else if(transArray[0].position.x == -2 && transArray[0].position.y == -1)
+        {
+            transArray[0].SetPositionAndRotation(new Vector3(-2, 1, 0), new Quaternion());
+            transArray[1].SetPositionAndRotation(new Vector3(2, -1, 0), new Quaternion());
+        }else if(transArray[0].position.x == -2 && transArray[0].position.y == 1)
+        {
+            transArray[0].SetPositionAndRotation(new Vector3(2, 1, 0), new Quaternion());
+            transArray[1].SetPositionAndRotation(new Vector3(-2, -1, 0), new Quaternion());
+        }
     }
 }
