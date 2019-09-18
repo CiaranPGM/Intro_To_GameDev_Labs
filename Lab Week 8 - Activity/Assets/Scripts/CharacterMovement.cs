4e995c27ff8836c26ec065fa85a4971c1e5570ad
Ciaran Mulholland <ciaranpgm@hotmail.com>
2019-09-18 23:54:42 +1000

    Added lab 7 and 8

---
+++ Lab Week 8 - Activity/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Vector3 movement;
+    private float movementSqrMagnitute;
+    public float walkSpeed = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMovementInput();
+        CharacterPosition();
+        CharacterRotation();
+        WalkingAnimation();
+        FootstepAudio();
+    }
+
+    void GetMovementInput()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+        movementSqrMagnitute = movement.sqrMagnitude;
+        Debug.Log(movement);
+    }
+
+    void CharacterPosition()
+    {
+        //gameObject.transform.position += movement * walkSpeed * Time.deltaTime;
+        gameObject.transform.Translate(movement * walkSpeed * Time.deltaTime);
+    }
+
+    void CharacterRotation()
+    {
+
+    }
+
+    void WalkingAnimation()
+    {
+
+    }
+
+    void FootstepAudio()
+    {
+
+    }
+}
